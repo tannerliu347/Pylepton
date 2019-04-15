0303a8101f587d57c1149111074270c73c7d8588
Tanner <tannerliu347@gmail.com>
2019-04-14 21:32:39 -0400

    "added client.loop_forever() to listen for message"

--- MQTTClientCameraTest.py
+++ MQTTClientCameraTest.py
@@ -1,31 +1,14 @@
 import time
 import paho.mqtt.client as mqtt
 from pylepton.LeptonCamera import LeptonCamera
-# FLIRModule has:
-    # temp.... -- returns list temps
-    # takeImg -- returns image array
-    # takeData
-        # calls temp... funcs or takeImg in some loop and returns values
-    # Camera class with _init_() that takes
-        # ask for test name (to be data header + date/time)
-        # Provide frame, ask if view is OK
-            # if not, ask for prompt to take new image once camera is physically repositioned
-        # Ask for points of interest, and their names
-        # Ask if thermocouples are connected, and their names
-        # Ask what value(s) to display on the lcd panel
-        # Ask if images files should be posted to MQTT topic
-        # Wait for start recording command
 
 print('Starting...\n')
 
+#initialize camera object
 testName = 'test1'
 pixel2Follow = {'First point' : [1,1]}
 camera1 = LeptonCamera(testName, pixel2Follow)
 saveLocally = False
-#camera1.saveData()
-#imgStr = open('rgb4.txt', 'r').read()
-
-#print(camera1.testName + '/' + camera1.testName + 'Lepton' +  + '.txt')
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
@@ -35,7 +18,7 @@ def on_connect(client, userdata, flags, rc):
     # reconnect then subscriptions will be renewed.
     #client.subscribe("asset/andon1/state")
     #takeOneImg()
-    
+
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
@@ -63,9 +46,6 @@ client.subscribe("Asset/FLIR1/Cntrl")
 # camera1.firstImg()
 # camera1.pickAreas()
 
-client.loop_start()  # start listening in a thread and proceed
-print("listening loop has started")
-
 def setTestName(tn):
     testName = tn
 
@@ -87,7 +67,7 @@ def takeOneImg():
     imgArr = camera1.getTempArr()
     imgStr = '\n'.join('\t'.join('%0.3f' % x for x in y) for y in imgArr)
     client.publish("asset/FLIR1/Imgs", payload = imgStr, qos=0, retain=False)
-    time.sleep(1) # delay needed for UKNOWN REASON
+    time.sleep(0.2) # delay needed for UKNOWN REASON
 
 def streamImg(sleepSeconds = 5):
     try:
@@ -106,8 +86,5 @@ def streamImg(sleepSeconds = 5):
     except KeyboardInterrupt:
             print('Process interrupted')
 
-takeOneImg();
-#streamImg();
 
-#finally:
- #   print('Done')p
+client.loop_forever()  # start listening in a thread and proceed
